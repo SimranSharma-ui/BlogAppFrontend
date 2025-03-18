005626ade1aefbcf6f8abd5a318c4d7daa19a307
SimranSharma-ui <sharmasimran0675@gmail.com>
2025-03-18 20:17:41 +0530

    fix bug

--- src/componants/AllBlogs.jsx
+++ src/componants/AllBlogs.jsx
@@ -11,7 +11,7 @@ const AllBlogs = () => {
 
   useEffect(()=>{
     fetchBlogs();
-  },[])
+  },[]);
  
   const truncateDescription = (description, length) => {
     if (description.length > length) {
